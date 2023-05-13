11e1581460308fc15a8ae188128f7282670eba97
Sumod K Mohan <sumodm@autoinfer.com>
2023-05-14 05:18:14 +0530

    Adding generated questions version

--- main.py
+++ main.py
@@ -16,12 +16,30 @@ with open("concepts.json", 'r') as f:
   concepts = json.load(f)
 with open("data/topic_graph.json", 'r') as f:
   data = json.load(f)
+with open("phy_subtop_qns.json", 'r') as f:
+  phy_subtop_qns = json.load(f)
 mapping = {"Physics":0, "Chemistry":1, "Maths":2, "Biology":3}
 
 def call_openai(prompt, max_tokens=1000, temp=0.3):
   completion = oai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [{"role":"user", "content":prompt}],
                                            temperature = temp, top_p = 1, n=1, max_tokens=max_tokens)
   return(completion.choices[0].message.content)
+
+
+@app.route('/get_subtopic_qns')
+def get_subtopic_qns():
+  board = request.args.get('board')
+  grade = request.args.get('grade')
+  subject = request.args.get('subject')
+  topics = request.args.get('topic')
+  topic_ids = request.args.get('topic_id')
+
+  if subject != 'Physics':
+      return {'None'}
+
+  results = {k:v for k,v in phy_subtop_qns.items() if k in topic_ids}
+  return jsonify(results)
+
 
 
 @app.route('/get_subtopic_qn')
@@ -40,8 +58,8 @@ def get_subtopic_qn():
   prompt0 = prompt0_tmpl.render(subject=subject, grade=grade, board=board,
                                 topic=topic, subtopics = subtopics,
                                 concepts = concepts[topic])
+  print(prompt0)
   question = call_openai(prompt0)
-  #print(prompt0)
   return jsonify({'status':'in-progress', 'type': 'question', 'question': question})
 
 
